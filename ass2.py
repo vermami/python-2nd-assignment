24b3f08d2f95da7ca8bd6aac3cd8aa8a740de661
Amit verma <103342132+vermami@users.noreply.github.com>
2025-08-07 08:24:45 +0530

    Add files via upload

---
+++ ass2.py
@@ -0,0 +1,15 @@
+# a = int(input("enter the number"))
+# if (a % 2 == 0):
+#     print(a,"is the even number")
+# else:
+#     print(a,"is the odd number")
+
+
+sum = 0
+for i in range(1,51):
+    print(i)
+    sum += i
+print("the sum of numbers from 1 to 50 is: ",sum)
+
+
+
---
+++ ass2t2.py
@@ -0,0 +1,5 @@
+sum = 0
+for i in range(1,51):
+    print(i)
+    sum += i
+print("the sum of numbers from 1 to 50 is: ",sum)
